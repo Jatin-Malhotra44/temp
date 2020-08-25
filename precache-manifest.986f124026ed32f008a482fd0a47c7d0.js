3d110bbb4b8962daf65b56dd2dcb36395e733791
unknown <malhotra.jatin1.jm@gmail.com>
2020-08-25 17:21:25 +0530

    Updates

---
+++ precache-manifest.986f124026ed32f008a482fd0a47c7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd1ac658d3a7fa37cce0d2ec251ca56",
+    "url": "/temp/index.html"
+  },
+  {
+    "revision": "c5e88dc2a5bc8fa6d14f",
+    "url": "/temp/static/css/main.7b1abb63.chunk.css"
+  },
+  {
+    "revision": "1c606deeb689b6a10935",
+    "url": "/temp/static/js/2.a76b13e5.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/temp/static/js/2.a76b13e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e88dc2a5bc8fa6d14f",
+    "url": "/temp/static/js/main.fc72bda0.chunk.js"
+  },
+  {
+    "revision": "2ee8cfd5cb28cca344d6",
+    "url": "/temp/static/js/runtime-main.10c37b35.js"
+  }
+]);
